0da3ededb0a7976777e289a47308d3f6d3a426f8
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-25 14:37:48 +0530

    main

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 app.listen(5000, ()=>{
     console.log("server started at port no. 3000")
 });
